ca4c1e87d6b9de6ba3d21c219859745168031c89
Noriel <norielsg1214@gmail.com>
2021-08-21 17:46:57 -0400

    Apply bootstrap theme

--- MVCApp11/App_Start/BundleConfig.cs
+++ MVCApp11/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace MVCApp11
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
