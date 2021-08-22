5dec84853056d84fe4644bb0bc8d146bc768bd16
Noriel <norielsg1214@gmail.com>
2021-08-22 19:32:52 -0400

    Add Validation

--- MVCApp11/Models/MembershipType.cs
+++ MVCApp11/Models/MembershipType.cs
@@ -9,7 +9,7 @@ namespace MVCApp11.Models
     public class MembershipType
     {
         public byte Id { get; set; }
-
+        [Required]
         public string Name { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
