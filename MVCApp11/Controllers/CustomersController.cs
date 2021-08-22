eabf7ab1f581bad649a4482d697e1ce13f05a3e7
Noriel <norielsg1214@gmail.com>
2021-08-22 17:55:29 -0400

    Add form to Add/Update customer

--- MVCApp11/Controllers/CustomersController.cs
+++ MVCApp11/Controllers/CustomersController.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVCApp11.Models;
+using MVCApp11.ViewModels;
 using System.Data.Entity;
 
 namespace MVCApp11.Controllers
 {
+
     public class CustomersController : Controller
     {
         private ApplicationDbContext _context;
@@ -22,9 +24,38 @@ namespace MVCApp11.Controllers
             _context.Dispose();
         }
 
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new CustomerFormViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (customer.Id == 0)
+                _context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
         public ViewResult Index()
         {
-            //var customers = _context.Customers.ToList();
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
 
             return View(customers);
@@ -32,13 +63,28 @@ namespace MVCApp11.Controllers
 
         public ActionResult Details(int id)
         {
-            //var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
             var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
 
             return View(customer);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel
+            {
+                Customer = customer,
+                MembershipTypes = _context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
     }
 }
---
+++ MVCApp11/ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MVCApp11.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp11.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
