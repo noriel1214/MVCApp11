a9a92023f320d08a897ee1bbfce772a9f850170f
Noriel <norielsg1214@gmail.com>
2021-08-23 11:15:31 -0400

    Built API for Movie model

--- MVCApp11/App_Start/MappingProfile.cs
+++ MVCApp11/App_Start/MappingProfile.cs
@@ -6,10 +6,19 @@ namespace MVCApp11.App_Start
 {
         public class MappingProfile : Profile
         {
-            public MappingProfile()
-            {
-                Mapper.CreateMap<Customer, CustomerDto>();
-                Mapper.CreateMap<CustomerDto, Customer>();
-            }
+        public MappingProfile()
+        {
+            // Domain to Dto
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Movie, MovieDto>();
+
+
+            // Dto to Domain
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<MovieDto, Movie>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
         }
+    }
     }
--- MVCApp11/Global.asax.cs
+++ MVCApp11/Global.asax.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+
 
 namespace MVCApp11
 {
@@ -12,7 +14,9 @@ namespace MVCApp11
     {
         protected void Application_Start()
         {
+
             AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
