b9df186927cda6637149ccde5f2ceb3ffc7af833
Noriel <norielsg1214@gmail.com>
2021-08-23 18:30:06 -0400

    Add Rentals & Move ApplicationDbContext class

---
+++ MVCApp11/Migrations/202108232222136_AddRentalModelAndExpandMovieModel.cs
@@ -0,0 +1,18 @@
+namespace MVCApp11.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddRentalModelAndExpandMovieModel : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Movies", "NumberAvailable", c => c.Byte(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Movies", "NumberAvailable");
+        }
+    }
+}
--- MVCApp11/Models/Movie.cs
+++ MVCApp11/Models/Movie.cs
@@ -28,5 +28,7 @@ namespace MVCApp11.Models
         [Display(Name = "Number in Stock")]
         [Range(1, 20)]
         public byte NumberInStock { get; set; }
+
+        public byte NumberAvailable { get; set; }
     }
 }
