281026fc21af05987135e221daf0efb341c58ccc
Noriel <norielsg1214@gmail.com>
2021-08-23 18:19:16 -0400

    Added Phone to Application User & Enabled GLIMPSE

--- MVCApp11/Models/AccountViewModels.cs
+++ MVCApp11/Models/AccountViewModels.cs
@@ -12,6 +12,9 @@ namespace MVCApp11.Models
         [Required]
         [Display(Name = "Driving License")]
         public string DrivingLicense { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Phone { get; set; }
     }
 
     public class ExternalLoginListViewModel
@@ -71,6 +74,10 @@ namespace MVCApp11.Models
         [Required]
         [Display(Name = "Driving License")]
         public string DrivingLicense { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Phone { get; set; }
 
         [Required]
         [EmailAddress]
