c34d524f3de86a481b07cb3e01772c2612328a0c
Noriel <norielsg1214@gmail.com>
2021-08-23 11:03:51 -0400

    Added Customer WebApi and implemented AutoMapper

---
+++ MVCApp11/App_Start/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using MVCApp11.Models;
+using MVCApp11.Dtos;
+
+namespace MVCApp11.App_Start
+{
+        public class MappingProfile : Profile
+        {
+            public MappingProfile()
+            {
+                Mapper.CreateMap<Customer, CustomerDto>();
+                Mapper.CreateMap<CustomerDto, Customer>();
+            }
+        }
+    }
